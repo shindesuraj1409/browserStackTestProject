44a735c828ce25749d48245cfbc084c3dfb03e29
suraj <shindesuraj1409@gmail.com>
2020-06-16 14:28:24 +0530

    testClass_forBrowserStack

---
+++ src/test/java/com/browserstack/stagetech/BrowserStackTest.java
@@ -0,0 +1,59 @@
+package com.browserstack.stagetech;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserStackTest {
+  public static final String USERNAME = "shindesuraj1";
+  public static final String AUTOMATE_KEY = "4q2zsp2JvvgUpf1LqhUA";
+  public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+  public WebDriver driver;
+  @BeforeMethod
+  public void setup() throws MalformedURLException
+  {
+	  DesiredCapabilities caps = new DesiredCapabilities();
+	    
+	    caps.setCapability("os", "Windows");
+	    caps.setCapability("os_version", "10");
+	    caps.setCapability("browser", "Chrome");
+	    caps.setCapability("browser_version", "80");
+	    
+	    caps.setCapability("name", "Chrome Test");
+	    
+	    driver = new RemoteWebDriver(new URL(URL), caps);
+	    driver.get("http://www.google.com");
+  
+  }
+  
+  @Test
+  public void serachOnBrowser()
+  {
+	  WebElement element = driver.findElement(By.name("q"));
+
+	    element.sendKeys("BrowserStack");
+	    element.submit();
+
+	    System.out.println(driver.getTitle());
+ 
+  }
+  
+  @AfterMethod
+  public void tearDown()
+  {
+	  driver.quit();
+  }
+           
+  
+}
